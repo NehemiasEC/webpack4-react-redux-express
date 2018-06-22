3f1d080c0582fb7e615d059351692ff06320d3bd
N Eliseo S Carranza <neli92santscz@gmail.com>
2018-06-21 23:33:50 -0400

    setup2

---
+++ src/client/index.js
@@ -0,0 +1,10 @@
+import React from  'react';
+import {render} from 'react-dom';
+
+
+const App = ()=>{
+    return <h1>Hello world fromreact</h1>
+}
+
+
+render(<App/>, document.getElementById("app"));
